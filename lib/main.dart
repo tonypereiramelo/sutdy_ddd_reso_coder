0bf96d403369df6cc0389f88840896cd3f35eb92
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-10-11 09:37:13 -0300

    fix(infra): :bug: correct bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
+import 'package:ddd_study/injection.dart';
+import 'package:ddd_study/presentation/core/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
-import 'infrastructure/navigation/navigation.dart';
-import 'infrastructure/navigation/routes.dart';
+import 'package:injectable/injectable.dart';
 
 void main() async {
-  var initialRoute = await Routes.initialRoute;
-  runApp(Main(initialRoute));
-}
-
-class Main extends StatelessWidget {
-  final String initialRoute;
-  Main(this.initialRoute);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: initialRoute,
-      getPages: Nav.routes,
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  configureInjection(Environment.prod);
+  runApp(const AppWidget());
 }
